4d9557736ad0425bd51f968eb2dfe79ae4c54dbb
cc4-13 <miguel.mendoza.11.1987@gmail.com>
2018-03-20 15:07:39 -0600

    cadenas

---
+++ 02cadena.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<string.h>
+#define NUM_HILOS 10
+
+pthread_t tabla_thr[NUM_HILOS];
+
+typedef struct{
+    int id;
+    char *cadena;
+}thr_param_t;
+
+thr_param_t param[NUM_HILOS];
+
+void *hilo(thr_param_t *p){
+    printf("%s %d \n", p->cadena, p->id);
+    pthread_exit(&p->id);
+} 
+
+int main(){
+    int i, *res;
+    printf ("Creando hilos... \n");
+    for (i = 0; i<NUM_HILOS; i++){
+        param[i].cadena=strdup("Hola, soy el hilo ");
+        param[i].id = i;
+        pthread_create(&tabla_thr,NULL, (void *)&hilo, (void *)&param[i]);
+    }
+
+    printf("Hilos creados. Esperando que terminen... \n");
+    for(i=0; i<NUM_HILOS; i++){
+        pthread_join(tabla_thr[i], (void *)&res);
+        printf("El hilo %d devolvio el valor %d \n", i,*res);
+    }
+    printf ("Todos los hilos finalizados... \n");
+    return 0;
+}
